c5c0abbaf6606c0a98b107ac9c4f78e016d4129e
Raj Arora <raj.arora@envistaco.com>
2022-11-09 13:32:12 -0500

    final commit before testing

--- kibana.tf
+++ kibana.tf
@@ -6,13 +6,5 @@ resource "helm_release" "kibana" {
   version    = "7.17.1"
   namespace  = "efk"
   verify     = false
-  set {
-    name  = "cluster.enabled"
-    value = "NodePort"
-  }
-  set {
-    name  = "service.nodePort"
-    value = "31000"
-  }
 }
 
